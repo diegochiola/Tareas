c3003c21d0a295ae251360c1dbd20ec3a6a59695
diega <diegochiola@gmail.com>
2024-02-07 23:24:10 +0100

    vista crear persona lista

--- views/persona/crear-persona.php
+++ views/persona/crear-persona.php
@@ -3,8 +3,30 @@
 ?>    
 <main class="container">
     <h1 class= "text-center"> Crear persona </h1>
-        <a href="<?=BASE_URL?>/views/persona/index.php"> Lista Personas </a>
-            
+        <a href="<?=BASE_URL?>/views/persona"> Lista Personas </a>
+         <br>
+         </br>  
+         <form id="FrmRestro">
+            <div class="mb-3">
+                <label for="txtNombre" class="form-label">Nombre</label>
+                <input type="text" class="form-control" id="txtNombre" name="txtNombre" placeholder="Nombres" required>
+            </div>
+            <div class="mb-3">
+                <label for="txtApellido" class="form-label">Apellido</label>
+                <input type="text" class="form-control" id="txtApellido" name="txtApelldio" placeholder="Apellidos" required>
+            </div>
+            <div class="mb-3">
+                <label for="txtTelefono" class="form-label">Telefono</label>
+                <input type="number" class="form-control" id="txtTelefono" name="txtTelefono" placeholder="Numero de Telefono" required>
+            </div>
+            <div class="mb-3">
+                <label for="txtEmail" class="form-label">Email</label>
+                <input type="email" class="form-control" id="txtEmail" name="txtEmail" placeholder="Correo Electronico" required>
+            </div>
+          
+            <button type="submit" class="btn btn-info"><i class="fa-regular fa-floppy-disk"></i> Guardar</button>
+</form>
+         
 </main>
 <?php
     require "../template/footer.php";
